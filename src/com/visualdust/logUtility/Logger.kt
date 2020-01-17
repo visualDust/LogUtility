c12154145c6e29960412bbdbecd701e413d6ae5a
VisualDust <VisualDust@outlook.com>
2020-01-17 09:53:34 +0800

    fixed some warnings

--- src/com/visualdust/logUtility/Logger.kt
+++ src/com/visualdust/logUtility/Logger.kt
@@ -5,19 +5,28 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStream
 import java.time.LocalDateTime
+import kotlin.random.Random
 import com.visualdust.logUtility.AttributedHtmlStr as AHS
 import com.visualdust.logUtility.AttributedHtmlStr.Companion.BuiltInColors as BIColor
 import com.visualdust.logUtility.AttributedShellStr as ASS
 import com.visualdust.logUtility.AttributedShellStr.Companion.BGs as ShellBG
 import com.visualdust.logUtility.AttributedShellStr.Companion.FGs as ShellFG
 import com.visualdust.logUtility.AttributedShellStr.Companion.Styles as ShellStyle
+import com.visualdust.logUtility.AttributedHtmlStr.Companion.BuiltInColors as WebColor
+
 
 class Logger {
-    val generator: Any
-    var timeout = DefaultTimeout
-        set(value) {
-            field = value
-        }
+    private val generator: Any
+    private var genShellBG = ShellBG.White
+    private var genHtmlBG = WebColor.White
+    private var timeout = DefaultTimeout
+
+    init {
+        while (genShellBG == ShellBG.White || genShellBG == ShellBG.Black)
+            genShellBG = ShellBG.values().elementAt(Random.nextInt(0, ShellBG.values().size))
+        while (genHtmlBG == WebColor.White)
+            genHtmlBG = WebColor.values().elementAt(Random.nextInt(0, WebColor.values().size))
+    }
 
     constructor(generator: Any) {
         this.generator = generator
@@ -32,7 +41,7 @@ class Logger {
 
     fun add(stream: OutStreamWithType, channel: Int) {
         if (!channelDictionary.containsKey(channel))
-            channelDictionary.put(channel, mutableListOf())
+            channelDictionary[channel] = mutableListOf()
         channelDictionary.getValue(channel).add(stream)
         write(stream.stream, "<p>---[---(LogUtility)---(github.com/VisualDust/LogUtility)---]--- got involved.</p>\n")
     }
@@ -41,7 +50,7 @@ class Logger {
 
     fun remove(stream: OutputStream, channel: Int) {
         for (streams in channelDictionary.getValue(channel)) {
-            if (stream.equals(streams.stream))
+            if (stream == streams.stream)
                 channelDictionary.getValue(channel).remove(streams)
         }
     }
@@ -82,54 +91,54 @@ class Logger {
     fun logFormatted(formattedStr: String) = logFormatted(formattedStr, 0)
 
     private fun logFormatted(str: String, type: LogType, channel: Int) {
-        var LDT = LocalDateTime.now()
+        val ldt = LocalDateTime.now()
         when (type) {
             LogType.Shell -> {
-                var processedStr = str
+                val processedStr = str
                     .replace(
                         "%gen%",
-                        ASS(generator).applyBG(ShellBG.Purple).applyFG(ShellFG.White).applyStyle(ShellStyle.Underline).toString()
+                        ASS(generator).applyBG(genShellBG).applyFG(ShellFG.White).applyStyle(ShellStyle.Underline).toString()
                     )
                     .replace("%true%", ASS("[√]").applyFG(ShellFG.Green).applyStyle(ShellStyle.Inverse).toString())
                     .replace("%false%", ASS("[×]").applyFG(ShellFG.Red).applyStyle(ShellStyle.Inverse).toString())
-                    .replace("%tif%", ASS(LDT).applyStyle(ShellStyle.Inverse).toString())
+                    .replace("%tif%", ASS(ldt).applyStyle(ShellStyle.Inverse).toString())
                     .replace(
                         "%tis%",
-                        ASS(LDT.dayOfYear + LDT.hour + LDT.minute + LDT.nano).applyStyle(ShellStyle.Inverse).toString()
+                        ASS(ldt.dayOfYear + ldt.hour + ldt.minute + ldt.nano).applyStyle(ShellStyle.Inverse).toString()
                     )
-                    .replace("%year%", ASS(LDT.year).applyStyle(ShellStyle.Inverse).toString())
-                    .replace("%mon%", ASS(LDT.month).applyStyle(ShellStyle.Inverse).toString())
-                    .replace("%dow%", ASS(LDT.dayOfWeek).applyStyle(ShellStyle.Inverse).toString())
-                    .replace("%dom%", ASS(LDT.dayOfMonth).applyStyle(ShellStyle.Inverse).toString())
-                    .replace("%doy%", ASS(LDT.dayOfYear).applyStyle(ShellStyle.Inverse).toString())
-                    .replace("%hour%", ASS(LDT.hour).applyStyle(ShellStyle.Inverse).toString())
-                    .replace("%min%", ASS(LDT.minute).applyStyle(ShellStyle.Inverse).toString())
-                    .replace("%sec%", ASS(LDT.second).applyStyle(ShellStyle.Inverse).toString())
-                    .replace("%nano%", ASS(LDT.nano).applyStyle(ShellStyle.Inverse).toString())
+                    .replace("%year%", ASS(ldt.year).applyStyle(ShellStyle.Inverse).toString())
+                    .replace("%mon%", ASS(ldt.month).applyStyle(ShellStyle.Inverse).toString())
+                    .replace("%dow%", ASS(ldt.dayOfWeek).applyStyle(ShellStyle.Inverse).toString())
+                    .replace("%dom%", ASS(ldt.dayOfMonth).applyStyle(ShellStyle.Inverse).toString())
+                    .replace("%doy%", ASS(ldt.dayOfYear).applyStyle(ShellStyle.Inverse).toString())
+                    .replace("%hour%", ASS(ldt.hour).applyStyle(ShellStyle.Inverse).toString())
+                    .replace("%min%", ASS(ldt.minute).applyStyle(ShellStyle.Inverse).toString())
+                    .replace("%sec%", ASS(ldt.second).applyStyle(ShellStyle.Inverse).toString())
+                    .replace("%nano%", ASS(ldt.nano).applyStyle(ShellStyle.Inverse).toString())
                 broadcast(processedStr, LogType.Shell, channel)
             }
             LogType.HTML -> {
-                var processedStr = "<p>" + str
+                val processedStr = "<p>" + str
                     .replace(
                         "%gen%",
                         AHS(generator).applyBG(Color(140, 0, 176)).applyFG(BIColor.White).toString()
                     )
                     .replace("%true%", AHS("[√]").applyBG(BIColor.Green).applyFG(BIColor.White).toString())
                     .replace("%false%", AHS("[×]").applyBG(BIColor.Red).applyFG(BIColor.White).toString())
-                    .replace("%tif%", AHS(LDT).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
+                    .replace("%tif%", AHS(ldt).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
                     .replace(
                         "%tis%",
-                        AHS(LDT.dayOfYear + LDT.hour + LDT.minute + LDT.nano).applyBG(BIColor.Gray).applyFG(BIColor.White).toString()
+                        AHS(ldt.dayOfYear + ldt.hour + ldt.minute + ldt.nano).applyBG(BIColor.Gray).applyFG(BIColor.White).toString()
                     )
-                    .replace("%year%", AHS(LDT.year).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
-                    .replace("%mon%", AHS(LDT.month).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
-                    .replace("%dow%", AHS(LDT.dayOfWeek).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
-                    .replace("%dom%", AHS(LDT.dayOfMonth).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
-                    .replace("%doy%", AHS(LDT.dayOfYear).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
-                    .replace("%hour%", AHS(LDT.hour).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
-                    .replace("%min%", AHS(LDT.minute).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
-                    .replace("%sec%", AHS(LDT.second).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
-                    .replace("%nano%", AHS(LDT.nano).applyBG(BIColor.Gray).applyFG(BIColor.White).toString()) + "</p>"
+                    .replace("%year%", AHS(ldt.year).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
+                    .replace("%mon%", AHS(ldt.month).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
+                    .replace("%dow%", AHS(ldt.dayOfWeek).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
+                    .replace("%dom%", AHS(ldt.dayOfMonth).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
+                    .replace("%doy%", AHS(ldt.dayOfYear).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
+                    .replace("%hour%", AHS(ldt.hour).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
+                    .replace("%min%", AHS(ldt.minute).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
+                    .replace("%sec%", AHS(ldt.second).applyBG(BIColor.Gray).applyFG(BIColor.White).toString())
+                    .replace("%nano%", AHS(ldt.nano).applyBG(BIColor.Gray).applyFG(BIColor.White).toString()) + "</p>"
                 broadcast(processedStr, LogType.HTML, channel)
             }
             LogType.Any -> {
@@ -144,29 +153,29 @@ class Logger {
     private fun broadcast(message: String, type: LogType, channel: Int) {
         if (!channelDictionary.containsKey(channel)) return
         if (channel != 0) broadcast(message, type, 0)
-        var subscriberList = channelDictionary.getValue(channel)
+        val subscriberList = channelDictionary.getValue(channel)
         for (subscriber in subscriberList)
             if (subscriber.type == type) write(subscriber.stream, message)
     }
 
     //todo rewrite this as an in queue writer and add exception resolvers
-    private fun write(stream: OutputStream, message: String){
-        stream.write(message.toByteArray())
+    private fun write(stream: OutputStream, message: String) {
+        Thread(StreamAttendant(stream, message, timeout)).start()
     }
 
     companion object {
         @JvmStatic
-        val DefauletLoggerName: String = "Logger"
+        val DefaultLoggerName: String = "Logger"
         private val OsProperties = System.getProperties()
         @JvmStatic
-        var StartUpTime = LocalDateTime.now()
+        var StartUpTime: LocalDateTime = LocalDateTime.now()
         @JvmStatic
         val DefaultTimeout = 500L
         @JvmStatic
         var LogSeparator = ">"
         @JvmStatic
         var DefaultLogFileName =
-            "${DefauletLoggerName}_" + "${StartUpTime.year}_" + "${StartUpTime.month}_" + "${StartUpTime.dayOfMonth}_Log_.html"
+            "${DefaultLoggerName}_" + "${StartUpTime.year}_" + "${StartUpTime.month}_" + "${StartUpTime.dayOfMonth}_Log_.html"
         private var channelDictionary = HashMap<Int, MutableList<OutStreamWithType>>()
     }
 
@@ -174,12 +183,11 @@ class Logger {
         Any, HTML, Shell
     }
 
-    private class StreamAttendant(var stream: OutputStream, message: String, var timeout: Long) : Runnable {
+    private class StreamAttendant
+        (var stream: OutputStream, message: String, var timeout: Long) : Runnable {
         init {
-            try {
-                stream.write(message.toByteArray());stream.flush();stream.close()
-            } catch (e: Exception) {
-            }
+            stream.write(message.toByteArray())
+            stream.flush()
         }
 
         override fun run() {
